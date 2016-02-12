9b1ca9b39ea3ca1e69673f9701eea5981bc0b997
aly_cheers17 <aly_cheers17@alys-mac.pk5001z>
2016-02-11 22:01:54 -0700

    02/111/2016 Manufacture control Functions Added

--- Leaving Planet Earth/src/byui/cit260/LeavingPlanetEarth/control/ManufactureControl.java
+++ Leaving Planet Earth/src/byui/cit260/LeavingPlanetEarth/control/ManufactureControl.java
@@ -11,4 +11,16 @@ package byui.cit260.LeavingPlanetEarth.control;
  */
 public class ManufactureControl {
     
+    public double calcBuildRocket(double engine, double gasoline, double wings, double paint, double oil){
+        
+        if (engine < 0 || gasoline < 0 || wings < 0 || paint < 0 || oil < 0) { // anything less than zero
+           return -1;
+        }
+        if (engine > 6 || gasoline > 60 || wings > 4 || paint > 10 || oil > 50) { // greater than the boundaries
+           return -1;
+       }
+        double buildRocket = engine + gasoline + wings + paint + oil;
+       
+       return buildRocket;
+    }
 }
