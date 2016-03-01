790392cb1b75c6043e458df9e354a9f4473f6b87
Curtis Lowe <lowecurt4@gmail.com>
2016-02-29 21:14:02 -0700

    Added files via upload

---
+++ Leaving Planet Earth/src/citbyui/cit260/LeavingPlanetEarth/view/ExploreLocationView.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.LeavingPlanetEarth.view;
+
+/**
+ *
+ * @author Curtis
+ * 
+ */
+public class ExploreLocationView {
+    private String promptMessage;
+
+    private void displayLocationInfo() {
+    System.out.println(
+        "\n*****You have entered a new location*****"
+        + "\n* Feel free to explore this area      *"
+        + "\n* and find out what opportunities   *"
+        + "\n* are available to you here.           *"
+    );
+    }
+}
